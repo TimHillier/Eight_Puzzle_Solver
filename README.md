eeb28aef1316fad4afec93d6e2de6b723d3efa83
Tim Hillier <Tim.r.hillier@gmail.com>
2019-01-13 16:11:19 -0800

    stuff

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Eight Puzzle Solver for CPSC 371
 
 ## Getting Started
 
-Should be able to give it a file of puzzles and will be able to solve it. 
+Should be able to give it a file of puzzles and will be able to solve them. 
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #Assignment 1 for AI
 import solver
+import numpy as np
 
 def main():
-    print("main")
     getPuzzle("puzzles.txt")
 
 #gets the next puzzle from the file.
@@ -10,7 +10,15 @@ def getPuzzle(fileName):
     with open(fileName) as f:
         content = f.readline()
         while content:
-            solver.all(content)
+            solver.all(twoDify(content))
             content = f.readline()
+
+
+#turns the 1d array into a 2d array
+def twoDify(puzzle):
+    p = puzzle.split()
+    return(np.reshape(p,(3,3)))
+    # return puzzle.reshape((3,3))
+
 
 main()
